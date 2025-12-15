26315867f289f6849a510db4f2ac8fe921ca3836
iammodernsage <hello@bhaveshverma.com>
2025-12-15 14:59:16 +0530

    fixes..

--- core-engine/internal_rule.h
+++ core-engine/internal_rule.h
@@ -3,12 +3,24 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <regex.h>
+#include <pcre.h>
 
-typedef struct internal_rule_t {
+/* typedef struct internal_rule_t {
           int id;
           char *pattern;
           char *location;
           int action;
-   } internal_rule_t;
+   } internal_rule_t; (commenting this because this is causing a hell lot of cofusions*/
 
-#endif
+typedef struct internal_rule_t {
+   waf_rule_t rule;   // original rule
+
+   regex_t regex;    //POSIX cmpiled regex
+   pcre *pcre_pattern;   //PCRE compiled pattern
+   pcre_extra *pcre_extra;  //PCRE study data
+
+   int is_regex;
+ } internal_rule_t;
+
+#endif //internal_rule_h
